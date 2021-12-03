cd55657a8008f345b11c6fc8edd4538fac93826d
Jonah Lawrence <jonah@freshidea.com>
2021-12-02 22:47:03 -0700

    refactoring

--- Day-03/part2.py
+++ Day-03/part2.py
@@ -1,64 +1,96 @@
-NUM_LEN = 12
+def extract_with_bit(numbers: list[str], index: int, bit_value: str) -> list[str]:
+    """
+    Extracts the numbers with the given bit value at the given index
 
-def extract_with_bit(numbers, index, bit_value):
+    Args:
+        numbers (list[str]): The list of numbers
+        index (int): The index of the bit
+        bit_value (str): The bit value to extract
+
+    Returns:
+        list[str]: The list of numbers with the given bit value at the given index
+    """
     return [number for number in numbers if number[index] == bit_value]
 
 
-def count_frequencies(numbers):
-    # for each of the NUM_LEN bits, count the frequency of each bit
-    freqs = [{"0": 0, "1": 0} for _ in range(NUM_LEN)]
+def count_frequencies(numbers: list[str], index: int) -> dict[str, int]:
+    """
+    Count the frequencies of 1's and 0's within a specific index of the list of numbers
 
-    for number in numbers:
-        number = number.strip()
-        for i in range(NUM_LEN):
-            freqs[i][number[i]] += 1
+    Args:
+        numbers (list[str]): A list of strings representing binary numbers
 
-    return freqs
+    Returns:
+        list[dict[str, int]]: A list of dictionaries with the frequencies of 1's and 0's
+    """
+    frequencies = {"0": 0, "1": 0}
+    for num in numbers:
+        frequencies[num[index]] += 1
+    return frequencies
 
 
-def most_common_bit(numbers, index):
-    freqs = count_frequencies(numbers)
-    return "1" if freqs[index]["1"] >= freqs[index]["0"] else "0"
+def most_common_bit(numbers: list[str], index: int) -> str:
+    """
+    Count the frequencies of 1's and 0's within a specific index of the list of numbers and return the most common bit
 
-def least_common_bit(numbers, index):
-    freqs = count_frequencies(numbers)
-    return "0" if freqs[index]["1"] >= freqs[index]["0"] else "1"
+    Args:
+        numbers (list[str]): A list of strings representing binary numbers
+        index (int): The index of the bit
+
+    Returns:
+        str: The most common bit value
+    """
+    freqs = count_frequencies(numbers, index)
+    return "1" if freqs["1"] >= freqs["0"] else "0"
+
+
+def least_common_bit(numbers: list[str], index: int) -> str:
+    """
+    Count the frequencies of 1's and 0's within a specific index of the list of numbers and return the least common bit
+
+    Args:
+        numbers (list[str]): A list of strings representing binary numbers
+        index (int): The index of the bit
+
+    Returns:
+        str: The least common bit value
+    """
+    freqs = count_frequencies(numbers, index)
+    return "0" if freqs["1"] >= freqs["0"] else "1"
+
+
+def extract_until_one_remains(numbers: list[str], most_common: bool) -> str:
+    """
+    Extracts the numbers with the given bit value at the current index until there is only one number left
+
+    The index is incremented by one after each iteration
+
+    Args:
+        numbers (list[str]): The list of numbers
+        most_common (bool): Whether to extract the most common bit or the least common bit
+
+    Returns:
+        str: The remaining number
+    """
+    index = 0
+    while len(numbers) > 1:
+        bit_value = (
+            most_common_bit(numbers, index)
+            if most_common
+            else least_common_bit(numbers, index)
+        )
+        numbers = extract_with_bit(numbers, index, bit_value)
+        index += 1
+    return numbers[0]
+
 
 if __name__ == "__main__":
     with open("input.txt", "r") as f:
-        data = f.readlines()
+        data = f.read().splitlines()
 
-    numbers = [line.strip() for line in data]
+    oxygen_generator_rating = extract_until_one_remains(data, True)
 
-    index = 0
+    co2_scrubber_rating = extract_until_one_remains(data, False)
 
-    while len(numbers) > 1:
-        bit = most_common_bit(numbers, index)
-        numbers = extract_with_bit(numbers, index, bit)
-        index = (index + 1) % NUM_LEN
-
-    print(numbers)
-
-    num1 = numbers[0]
-
-    numbers = [line.strip() for line in data]
-
-    index = 0
-
-    while len(numbers) > 1:
-        bit = least_common_bit(numbers, index)
-        numbers = extract_with_bit(numbers, index, bit)
-        index = (index + 1) % NUM_LEN
-        print(numbers)
-
-    print(numbers)
-
-    num2 = numbers[0]
-
-    # convert num1 and num2 from binary string to decimal
-    num1 = int(num1, 2)
-    num2 = int(num2, 2)
-
-    # multiply the two numbers
-    print(num1 * num2)
-
+    # convert to decimal and multiply together
+    print(int(oxygen_generator_rating, 2) * int(co2_scrubber_rating, 2))

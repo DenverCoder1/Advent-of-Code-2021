73eead211453754f1e2c51067639447d2881551e
Jonah Lawrence <jonah@freshidea.com>
2021-12-08 15:30:11 -0700

    refactor: simplified code

--- Day-08/part2.py
+++ Day-08/part2.py
@@ -51,28 +51,42 @@ For each entry, determine all of the wire/segment connections and decode the fou
 
 import os
 import itertools
+from typing import Optional
+
+
+def alpha_sort(pattern: str) -> str:
+    """
+    Sort the pattern alphabetically
+
+    Args:
+        pattern (str): pattern to be sorted
+
+    Returns:
+        The sorted pattern.
+    """
+    return "".join(sorted(pattern))
 
 
 def substitute(pattern: str, mapping: dict[str, str]) -> str:
     """
-    Substitute each character in pattern with the corresponding value in mapping and sort alphabetically
+    Substitute each character in the pattern with the corresponding value in the mapping and sort alphabetically
 
     Args:
         pattern (str): pattern to be substituted
-        mapping (dict[str, str]): mapping of letters to letters in standard configuration
+        mapping (dict[str, str]): mapping of letters
 
     Returns:
         The substituted pattern.
     """
-    return "".join(sorted(mapping[char] for char in pattern))
+    return alpha_sort(mapping[char] for char in pattern)
 
 
 def identify_digits(
     patterns: dict[int, str],
     unknown_patterns: list[str],
     wire_map: dict[str, str],
-    identified: list[int],
-) -> list[str]:
+    identified: list[str],
+) -> Optional[list[str]]:
     """
     Given a list of patterns, recursively identify which digits correspond to which patterns with backtracking.
 
@@ -80,7 +94,7 @@ def identify_digits(
         patterns (dict[int, str]): mapping of digit to pattern in standard configuration
         unknown_patterns (list[str]): list of patterns that have not yet been identified
         wire_map (dict[str, str]): mapping of letters to letters in standard configuration
-        identified (list[int]): list of digits that have already been identified
+        identified (list[str]): list of digits that have already been identified
 
     Returns:
         list of digits that correspond to the unknown patterns
@@ -88,37 +102,38 @@ def identify_digits(
     if not unknown_patterns:
         return identified
 
-    unidentifed_patterns = {
-        num: pattern for num, pattern in patterns.items() if num not in identified
-    }
-
+    # take the first unknown pattern and try to identify it
     unknown_pattern = unknown_patterns[0]
 
-    for num, pattern in unidentifed_patterns.items():
-        if len(pattern) == len(unknown_pattern):
-            unknown_wires = [w for w in unknown_pattern if w not in wire_map.keys()]
-            unknown_in_pattern = [w for w in pattern if w not in wire_map.values()]
-            for perm in itertools.permutations(unknown_wires):
-                mapping = {
-                    **dict(zip(unknown_wires, perm)),
-                    **dict(zip(perm, unknown_in_pattern)),
-                }
-                # check if mapping contradicts wire_map
-                if substitute(unknown_pattern, {**wire_map, **mapping}) != pattern:
-                    continue
-                identified_copy = identified.copy()
-                # recursively identify digits
-                identified = identify_digits(
-                    patterns,
-                    unknown_patterns[1:],
-                    {**wire_map, **mapping},
-                    identified + [num],
-                )
-                if identified:
-                    return identified
-                identified = identified_copy
+    for num, pattern in patterns.items():
+        # skip if the digit has been identified already or the patterns are different lengths
+        if num in identified or len(pattern) != len(unknown_pattern):
+            continue
+        # extract the letters from unknown_pattern that have not been identified yet
+        unknown_wires = [w for w in unknown_pattern if w not in wire_map.keys()]
+        # extract the letters from pattern that don't have a corresponding wire yet
+        unknown_in_pattern = [w for w in pattern if w not in wire_map.values()]
+        # skip if the number of unknown wires in the patterns are not equal
+        if len(unknown_wires) != len(unknown_in_pattern):
+            continue
+        for perm in itertools.permutations(unknown_wires):
+            mapping = dict(zip(perm, unknown_in_pattern))
+            # skip permutation if mapping contradicts wire_map
+            if substitute(unknown_pattern, {**wire_map, **mapping}) != pattern:
+                continue
+            # recursively identify digits
+            result = identify_digits(
+                patterns,
+                unknown_patterns[1:],
+                {**wire_map, **mapping},
+                identified + [num],
+            )
+            # if the result is not None, return it, otherwise backtrack
+            if result:
+                return result
 
-    return []
+    # if no mapping was found, return None
+    return None
 
 
 def main():
@@ -126,7 +141,7 @@ def main():
         data = f.read().splitlines()
 
     """
-    Patterns follow the following configuration:
+    Patterns following the following configuration:
 
      aaaa 
     b    c
@@ -137,32 +152,40 @@ def main():
      gggg 
     """
     patterns = {
-        0: "abcefg",
-        1: "cf",
-        2: "acdeg",
-        3: "acdfg",
-        4: "bcdf",
-        5: "abdfg",
-        6: "abdefg",
-        7: "acf",
-        8: "abcdefg",
-        9: "abcdfg",
+        "0": "abcefg",
+        "1": "cf",
+        "2": "acdeg",
+        "3": "acdfg",
+        "4": "bcdf",
+        "5": "abdfg",
+        "6": "abdefg",
+        "7": "acf",
+        "8": "abcdefg",
+        "9": "abcdfg",
     }
 
     total = 0
 
     for line in data:
-        before, after = line.split(" | ")
-        before = before.split()
-        after = after.split()
-        # sort each pattern alphabetically
-        before = sorted(["".join(sorted(p)) for p in before], key=len)
-        identified = identify_digits(patterns, before, {}, [])
-        code = dict(zip(before, identified))
-        output = ""
-        for pattern in after:
-            pattern = "".join(sorted(pattern))
-            output += str(code[pattern])
+        # parse the line
+        all_patterns, output_patterns = line.split(" | ")
+        all_patterns = all_patterns.split()
+        output_patterns = output_patterns.split()
+
+        # sort each pattern alphabetically and sort the list by length
+        all_patterns = sorted(map(alpha_sort, all_patterns), key=len)
+
+        # identify digits for each pattern
+        identified = identify_digits(patterns, all_patterns, {}, [])
+        assert identified is not None
+
+        # map each pattern to its corresponding digit
+        code = dict(zip(all_patterns, identified))
+
+        # decode the output pattern
+        output = "".join(code[alpha_sort(pattern)] for pattern in output_patterns)
+
+        # add the output value to the total
         total += int(output)
 
     print(total)
